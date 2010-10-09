f03d30364010982b89577cb61c60a42f73cd0629
svart <dimashink@gmail.com>
2010-10-09 18:27:00 +0400

    Исправление случайностей.

--- tests/counting_genres_v1.py
+++ tests/counting_genres_v1.py
@@ -9,43 +9,29 @@ from ID3v1 import ID3v1
 #####################Testing reading tags##########################
 dirToScan = "/media/LVM/Music"
 
-mp3List = []
-fileExtensions = {}
+genres = {}
+counter = 0
 
 def ScanDirectory (dirPath):
+    global counter
     if os.path.isdir(dirPath):
         fileList = glob.glob(dirPath+"/*")
         for everyFile in fileList:
             ScanDirectory(everyFile)
     if os.path.isfile(dirPath):
-        if dirPath[-4] == ".":
-            if dirPath[-3:] in fileExtensions:
-                fileExtensions[dirPath[-3:]] += 1
-            else:
-                fileExtensions[dirPath[-3:]] = 1
-        elif dirPath[-5] == ".":
-            if dirPath[-4:] in fileExtensions:
-                fileExtensions[dirPath[-4:]] += 1
-            else:
-                fileExtensions[dirPath[-4:]] = 1
         if dirPath[-3:] == "mp3":
-            mp3List.append(dirPath)
-            
             f = open(dirPath, "rb")
             tag = ID3v1()
             try:
                 tag.GetTag(f)
-            except ValueError:
-                print (dirPath)
-                exit(0)
-            print (tag.genre)
+            except IOError:
+                print ("No tag: ", dirPath)
+            else:
+                genres[tag.genre] = genres.get(tag.genre, 0) + 1
             f.close()
-
-            #print (len(mp3List), "\r", end="")
-            #print (mp3List[len(mp3List)-1], "\r", end="")
-
+            counter = counter + 1
+            print (counter, "\r", end="")
 
 ScanDirectory(dirToScan)
 
-print(len(mp3List))
-
+print("\n", genres)
